2f152f06c791dfc5d20f6c442f1e73ff648407bd
Sebastian <Sebastian@DESKTOP-FC2DPB2>
2025-03-03 10:15:31 +0100

    add some note

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    #some note
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytestsite.settings')
     try:
         from django.core.management import execute_from_command_line
